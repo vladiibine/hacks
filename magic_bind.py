bc64b6bb3e93e196159a4f733dbc646fa79b2488
vladiibine <vlad.ardelean@3pillarglobal.com>
2014-05-31 16:40:16 +0300

    fixed algorithm and added extra tests

--- magic_bind.py
+++ magic_bind.py
@@ -1,3 +1,5 @@
+from copy import deepcopy
+from itertools import chain
 from types import FunctionType as Func, MethodType as Meth
 from inspect import getargspec
 
@@ -21,9 +23,9 @@ class MagicBind(object):
     ...         return x, y
     ...     def three_positional(self, x, y, z):
     ...         return x, y, z
-    ...     def six_positional_vargs_kwargs(self, x, y, z, a=98, b=43, d=-112,
-    ...                                         *owkargs, **ownkwargs):
-    ...         return x,y,z,a,b,d,owkargs, ownkwargs
+    ...     def sid_pox_defs_args_kwargs(self, x, y, z, a=98, b=43, d=-112,
+    ...                                     e=-9, *owkargs, **ownkwargs):
+    ...         return x,y,z,a,b,d,e,owkargs, ownkwargs
     ...     def defaults(self, x=1, y=2, z=3):
     ...         return x, y, z
     ...     def args_kwargs(self, x, y, *args, **kwargs):
@@ -37,41 +39,43 @@ class MagicBind(object):
     ...         return x, y, z
 
     >>> t = Test()
-    >>> mb2 = MagicBind(t, 1, 2, 3, x=221, z=5466, b=11111, a='first',d='second', third='thirsd')
-    >>> mb2.six_positional_vargs_kwargs(118, 118, y=17,  delta='delta')
-    (221, 17, 5466, 'first', 111111, 'second', (1, 2, 3), {'third': 'thirsd'})
-    """
-    # >>> mb = MagicBind(t, x=10)
-    # >>> mb.a
-    # 100
-    # >>> mb.no_args() is t
-    # True
-    # >>> mb.two_positional(20)
-    # (10, 20)
-    # >>> mb.three_positional(20, 30)
-    # (10, 20, 30)
-    # >>> mb.defaults(20)
-    # (10, 20, 3)
-    # >>> mb.defaults()
-    # (10, 2, 3)
-    # >>> mb.defaults(y=20)
-    # (10, 20, 3)
-    # >>> mb.args_kwargs(20, 30, 40, z=50, w=60)
-    # (10, 20, (30, 40), [('w', 60), ('z', 50)])
-    # >>> mb.only_args_kwargs()
-    # ((), [])
-    # >>> mb.args_kwargs(20, 30, x=50, w=60) # doctest:+IGNORE_EXCEPTION_DETAIL
-    # Traceback (most recent call last):
-    # TypeError:
-    # >>> mb.only_args_kwargs(20, 30, 40, z=50, w=60)
-    # ((20, 30, 40), [('w', 60), ('z', 50)])
-    # >>> mb.only_args_kwargs(10, 20, x=30, y=40)
-    # ((10, 20), [('x', 30), ('y', 40)])
-    # >>> mb.static(20, 30)
-    # (10, 20, 30)
-    # >>> t.clble = t
-    # >>> mb.clble(20)
-    # (10, 20, 3)
+    >>> mb2 = MagicBind(t, x=221, z=5466, b=11111, a='first',
+    ... d='second', third='thirsd')
+    >>> mb2.sid_pox_defs_args_kwargs(-999, y=17, delta='del')
+    (221, 17, 5466, 'first', 11111, 'second', -999, (), {'delta': 'del'})
+    >>> mb = MagicBind(t, x=10)
+    >>> mb.a
+    100
+    >>> mb.no_args() is t
+    True
+    >>> mb.two_positional(20)
+    (10, 20)
+    >>> mb.three_positional(20, 30)
+    (10, 20, 30)
+    >>> mb.defaults(20)
+    (10, 20, 3)
+    >>> mb.defaults()
+    (10, 2, 3)
+    >>> mb.defaults(y=20)
+    (10, 20, 3)
+    >>> mb.args_kwargs(20, 30, 40, z=50, w=60)
+    (10, 20, (30, 40), [('w', 60), ('z', 50)])
+    >>> mb.only_args_kwargs()
+    ((), [])
+    >>> mb.args_kwargs(20, 30, x=50, w=60) # doctest:+IGNORE_EXCEPTION_DETAIL
+    Traceback (most recent call last):
+    TypeError:
+    >>> mb.only_args_kwargs(20, 30, 40, z=50, w=60)
+    ((20, 30, 40), [('w', 60), ('z', 50)])
+    >>> mb.only_args_kwargs(10, 20, x=30, y=40)
+    ((10, 20), [('x', 30), ('y', 40)])
+    >>> mb.static(20, 30)
+    (10, 20, 30)
+    >>> mb(1, 2)
+    (10, 1, 2)
+    >>> t.clble = t
+    >>> mb.clble(20)
+    (10, 20, 3)"""
     def __init__(self, obj, *args, **kwargs):
         self._obj = obj
         self._kwargs = kwargs
@@ -99,42 +103,38 @@ class MagicBind(object):
         ovw_kwargs = (super(MagicBind, self).__getattribute__('_kwargs'))
         ovw_args = super(MagicBind, self).__getattribute__('_args')
         orig_clble = super(MagicBind, self).__getattribute__('_clble')
-        # ok_args = merge_args(orig_clble, ovw_kwargs, args)
         ok_args, ok_kwargs = (
-            totally_merge_args(orig_clble, ovw_args, ovw_kwargs, args, kwargs))
+            merge_args_better(orig_clble, ovw_args, ovw_kwargs, args, kwargs))
         return orig_clble(*ok_args, **ok_kwargs)
 
-def totally_merge_args(callable_, ovw_args, ovw_dict, add_args, add_dict):
-    """Given the callable, return (args, kwargs) to be unpacked when calling it
-    """
-    ok_args=[]
-    ok_kwargs={}
 
-    begin_index = 0 if isinstance(callable_, Func) else 1
+def merge_args_better(clble, prio_args, prio_kwargs, add_args, add_kwargs):
+    ok_args = []
+    ok_kwargs = {}
+    prio_args = list(deepcopy(prio_args))
+    prio_kwargs = deepcopy(prio_kwargs)
+    add_args = list(add_args)
 
-    for argname in getargspec(callable_)[0][begin_index:]:
-        if argname in ovw_dict:
-            ok_args
+    begin_index = 0 if isinstance(clble, Func) else 1
 
+    for argname in getargspec(clble)[0][begin_index:]:
+        if argname in prio_kwargs:
+            ok_args.append(prio_kwargs.pop(argname))
+        elif argname in add_kwargs:
+            ok_args.append(add_kwargs.pop(argname))
+        elif prio_args:
+            ok_args.append(prio_args.pop(0))
+        elif add_args:
+            ok_args.append(add_args.pop(0))
 
+    ok_args.extend(chain(prio_args, add_args))
+    if getargspec(clble)[2]:
+        # also append prio_dict to transmit MagicBind initial arguments
+        # as kws, if the underlying callable doesn't have args with that name
+        ok_kwargs.update(add_kwargs)
 
-def merge_args(callable_, overwriting, args):
-    """Return `vargs` where `vargs` represents a list of values, to be unpacked
-        when calling the `callable_`
-    """
-    vargs = []
-    args = list(args)
+    return ok_args, ok_kwargs
 
-    begin_index = 0 if isinstance(callable_, Func) else 1
-
-    for argname in getargspec(callable_)[0][begin_index:]:
-        if argname in overwriting:
-            vargs.append(overwriting[argname])
-        elif len(args) > 0:
-            vargs.append(args.pop(0))
-
-    vargs.extend(args)
-    return vargs
 
 if __name__ == '__main__':
     import doctest
